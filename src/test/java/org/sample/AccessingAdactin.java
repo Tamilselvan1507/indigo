050da692887675e0ef56c156fcfe344b4ff32d43
Tamilselvan1507 <tamilselvan150797@gmail.com>
2023-01-23 00:33:53 +0530

    update adactin hotel page by tamil

--- src/test/java/org/sample/AccessingAdactin.java
+++ src/test/java/org/sample/AccessingAdactin.java
@@ -1,6 +1,7 @@
 package org.sample;
 
 import org.baseclasses.Baseclass;
+import org.junit.Test;
 import org.testng.annotations.AfterClass;
 
 public class AccessingAdactin extends Baseclass {
@@ -10,6 +11,16 @@ public class AccessingAdactin extends Baseclass {
 		launchbrowser();
 		geturl("https://adactinhotelapp.com/index.php");
 		maximizewindow();
+	}
+	
+	@Test
+	private void test2() {
+		// TODO Auto-generated method stub
+
+	}
+	@AfterClass
+	private void AfterClass() {
+		closeacurrentwindow();
 	}
 
 }
